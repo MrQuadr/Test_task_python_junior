ef28636e15c2788a32655a4a3486875684b94429
Sergey R <sergay.rom@gmail.com>
2024-11-07 20:29:09 +0300

    task 3

--- task/3_reverse_string.py
+++ task/3_reverse_string.py
@@ -1,5 +1,7 @@
-def reverse_string(s):
+def reverse_string(s: str) -> str:
+    return s[::-1]
     
 
 if __name__ == "__main__":
-    pass
+    text = "Hello, hello world! World. It's a beautiful world, isn't it?"
+    print(reverse_string(text))
