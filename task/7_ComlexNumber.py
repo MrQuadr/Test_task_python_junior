978cfac17b855d8009d60acb4bbecdf9142d26ca
Sergey R <sergay.rom@gmail.com>
2024-11-07 18:33:24 +0300

    create task 7

--- task/7_ComlexNumber.py
+++ task/7_ComlexNumber.py
@@ -1,23 +1,34 @@
 class ComplexNumber:
-    def __init__(self, real, imag):
-        # Ваш код здесь
+    def __init__(self, real, imag) -> None:
+        self.real = real
+        self.imag = imag
         pass
 
     def __add__(self, other):
-        # Ваш код здесь
-        pass
+        return ComplexNumber((self.real+other.real),\
+                             (self.imag+other.imag))
 
     def __sub__(self, other):
-        # Ваш код здесь
-        pass
+        return ComplexNumber((self.real-other.real),\
+                             (self.imag-other.imag))
 
     def __mul__(self, other):
-        # Ваш код здесь
-        pass
+        result_real = self.real*other.real -\
+                        self.imag*other.imag
+        result_imag = self.real*other.imag +\
+                        self.imag*other.real
+        return ComplexNumber(result_real, result_imag)
 
     def __truediv__(self, other):
-        # Ваш код здесь
-        pass
+        denominator = other.real**2 + other.imag**2
+        result = self * ComplexNumber(other.real, (-1)*other.imag)
+        result.real /= denominator
+        result.imag /= denominator
+        return result
 
 if __name__ == "__main__":
+    a = ComplexNumber(5,9)
+    b = ComplexNumber(7,3)
+    c = a / b
+    print(c.real, c.imag)
     pass
