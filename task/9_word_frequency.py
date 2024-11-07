2a86920487c27995d5815eef1579196498f8c6b2
Sergey R <sergay.rom@gmail.com>
2024-11-07 20:00:57 +0300

    update task 9

--- task/9_word_frequency.py
+++ task/9_word_frequency.py
@@ -1,6 +1,19 @@
-def word_frequency(s):
-    # Ваш код здесь
-    pass
+def word_frequency(s: str) -> dict:
+    signs = ",.!?;:"
+    s_dict = {}
+    
+    for char in signs:
+        s = s.replace(char, "")
+    s = s.lower().split(" ")
+
+    for i in s:
+        if s_dict.get(i) is None:
+            s_dict[i] = 1
+        else:
+            s_dict[i] += 1
+    return s_dict
 
 if __name__ == "__main__":
+    text = "Hello, hello world! World. It's a beautiful world, isn't it?"
+    print(word_frequency(text))
     pass
