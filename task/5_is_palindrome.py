995612f26db03677178ed17d58dd91dc4dce1c6d
Sergey R <sergay.rom@gmail.com>
2024-11-07 20:20:09 +0300

    task 5

--- task/5_is_palindrome.py
+++ task/5_is_palindrome.py
@@ -1,6 +1,16 @@
-def is_palindrome(s):
-    # Ваш код здесь
-    pass
+def is_palindrome(s: str) -> bool:
+    s = s.lower().replace(" ", "")
+    
+    for i, j in enumerate(s[:(len(s)//2+1)], start=1):
+        if j != s[-i]:
+            return False
+    return True
+    
 
 if __name__ == "__main__":
-    pass
+    text = "A man a plan a canal Panama"
+    print(is_palindrome(text))
+    text = "A man a plan s canal Panama"
+    print(is_palindrome(text))
+    text = "A man a plan a Lanal Panama"
+    print(is_palindrome(text))
