038e756c840938e8def8a04829c482d0598f96f0
Sergey R <sergay.rom@gmail.com>
2024-11-07 20:24:54 +0300

    task 5

--- task/5_is_palindrome.py
+++ task/5_is_palindrome.py
@@ -1,6 +1,14 @@
 def is_palindrome(s: str) -> bool:
     s = s.lower().replace(" ", "")
-    return s == s[::-1]
+    
+    left, right = 0, len(s) - 1
+    while left < right:
+        if s[left] != s[right]:
+            return False
+        left += 1
+        right -= 1
+    
+    return True
     
 
 if __name__ == "__main__":
